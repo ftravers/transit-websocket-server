a7342ef597c51b8eb196f91b0963895611336a17
Fenton Travers <fenton.travers@gmail.com>
2017-09-24 10:19:22 -0700

    use transit to encode into json

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject fentontravers/websocket-server "0.4.12-SNAPSHOT"
+(defproject fentontravers/transit-websocket-server "0.4.12-SNAPSHOT"
   :description "WebSocket Server Library"
   :url "https://github.com/ftravers/websocket-server"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
                  [http-kit "2.2.0"]
+                 [com.cognitect/transit-clj "0.8.300"]
                  [com.taoensso/timbre "4.8.0"]
                  [org.clojure/core.async "0.2.395"]]
   :target-path "target/%s"
--- src/websocket_server/core.clj
+++ src/websocket_server/core.clj
@@ -1,6 +1,21 @@
 (ns websocket-server.core
   (:require [org.httpkit.server :as http]
-            [taoensso.timbre :as log]))
+            [taoensso.timbre :as log]
+            [cognitect.transit :as transit])
+  (:import [java.io ByteArrayInputStream ByteArrayOutputStream]))
+
+(defn json->edn [json]
+  (-> json
+      .getBytes
+      ByteArrayInputStream.
+      (transit/reader :json)
+      transit/read))
+
+(defn edn->json [edn]
+  (let [out (ByteArrayOutputStream. 4096)
+        writer (transit/writer out :json)]
+    (transit/write writer edn)
+    (.toString out)))
 
 (defn websocket-server [cb req]
   (http/with-channel req channel
@@ -9,14 +24,15 @@
      (fn [status] (log/debug (str "Websocket channel closed with status: " status))))
     (http/on-receive
      channel
-     (fn [data]
+     (fn [json-data]
        (if (http/websocket? channel)
-         (let [resp (cb data)]
+         (let [data (json->edn json-data)
+               resp (cb data)
+               json-resp (edn->json resp)]
            (log/debug "RECV: " data)
            (log/debug "RESP: " resp)
-           (http/send! channel resp)))))))
+           (http/send! channel json-resp)))))))
 
 (defn start-ws-server [port callback]
   (http/run-server (partial websocket-server callback)
                    {:port port}))
-
