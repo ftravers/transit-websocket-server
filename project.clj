48084238314b603727e86fbfbaad9f739c0691fc
Fenton Travers <fenton.travers@gmail.com>
2017-09-24 10:20:31 -0700

    Version 0.4.12

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fentontravers/transit-websocket-server "0.4.12-SNAPSHOT"
+(defproject fentontravers/transit-websocket-server "0.4.12"
   :description "WebSocket Server Library"
   :url "https://github.com/ftravers/websocket-server"
   :license {:name "Eclipse Public License"
